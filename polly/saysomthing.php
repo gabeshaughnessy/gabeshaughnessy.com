4e3b02a77847c7ca580758d78e97e850a4750400
Gabe Shaughnessy <gabeshaughnessy@gmail.com>
2017-06-22 19:03:55 +0000

    polly api stuff

---
+++ polly/saysomthing.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$sharedConfig = [
+    'region'  => 'us-west-2',
+    'version' => 'latest'
+];
+
+$sdk = new Aws\Sdk($sharedConfig);
+
+$voices = array('Geraint','Gwyneth','Mads','Naja','Hans','Marlene','Nicole','Russell','Amy','Brian','Emma','Raveena','Ivy','Joanna','Joey','Justin','Kendra','Kimberly','Salli','Conchita','Enrique','Miguel','Penelope','Chantal','Celine','Mathieu','Dora','Karl','Carla','Giorgio','Mizuki','Liv','Lotte','Ruben','Ewa','Jacek','Jan','Maja','Ricardo','Vitoria','Cristiano','Ines','Carmen','Maxim','Tatyana','Astrid','Filiz'');
+if(isset($_GET['voice']) && in_array(filter_input(INPUT_GET,'voice'), $voices)){
+ $voice = $_GET['voice'];
+}else{
+ $voice = 'Russell';
+}
+$pollyClient = $sdk->createPolly();
+
+$result = $pollyClient->synthesizeSpeech([
+    'OutputFormat' => 'mp3', // REQUIRED
+    'Text' => 'Hi there, this was created with the PHP SDK for AWS', // REQUIRED
+    'TextType' => 'text',
+    'VoiceId' => $voice
+]); 
+$mp3 = $result['AudioStream']->__toString();
+file_put_contents('sdk.mp3', $mp3);
+echo '<video controls="" autoplay="" name="media"><source src="/sdk.mp3" type="audio/mpeg"></video>';
+?>
